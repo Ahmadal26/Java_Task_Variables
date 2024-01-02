5f541c6913b1b2d6db540f447c8dc521adab96a0
Ahmad Y Y M Kh Musallam <83317@kfh.com>
2024-01-02 15:45:30 +0300

    Done with the Arithmetic task

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        // Task Variables
 String firstName = "Ahmad";
 String lastName = "Musallam";
 int age = 24; 
@@ -27,8 +28,15 @@ System.out.println(amount3+" " +"plus "+amount3 +" makes" +" "+ (amount3+amount3
 System.out.println( "My name is"+fullName.substring(0,5).toUpperCase()+" and my last name length is "+fullName.substring(6).length());
 System.out.println(fullName.substring(6).startsWith("d"));
 
+// Task Arithmetics
 
-
+double amountKWD =86;
+double amountUS = amountKWD*3.25;
+double amountUS2 = amountUS/3.25;
+double amountEuro= amountKWD*2.97;
+ System.out.println((int)amountKWD+ " KWD = " + amountUS+" USD");
+  System.out.println(amountUS+ " USD = " + amountUS2+" KWD");
+  System.out.println(amountEuro+ " Euro = " + amountKWD+" KWD");
 
     }
     
